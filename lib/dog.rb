600cf642f88e5415c14625fe86750aef1a41ef11
Parker Williamson <parker.williamson@gmail.com>
2020-06-04 22:16:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,24 @@ class Dog
     @name = name 
     @breed = breed 
   end
+  
+  def self.create_table 
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs ( 
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      );
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table 
+    sql = <<-SQL 
+      DROP TABLE IF EXISTS dog 
+    SQL
+    
+    DB[DB:conn].execute(sql)
+  end
 end
