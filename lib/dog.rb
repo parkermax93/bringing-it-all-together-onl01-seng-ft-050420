186996cc4d738b45dff93bceb47d2585b93944d5
Parker Williamson <parker.williamson@gmail.com>
2020-06-04 23:12:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,9 +29,8 @@ class Dog
   end
   
   def self.create(name:, breed:)
-    dog = Dog.new(name, breed)
+    dog = Dog.new(name: "#{name}", breed: "#{breed}")
     dog.save
-    dog
   end
   
   def save 
