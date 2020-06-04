ef373b300fb10aeaff772e3f4e42ecd73dcdb5de
Parker Williamson <parker.williamson@gmail.com>
2020-06-04 23:37:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,4 +73,9 @@ class Dog
     result = DB[:conn].execute(sql, name)[0]
     Dog.new(id: result[0], name: result[1], breed: result[2])
   end
+  
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.album)
+  end
 end
