dd6fbaecc9769678cf23cfb9e39f286c4e1f8069
Parker Williamson <parker.williamson@gmail.com>
2020-06-04 22:12:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,9 @@ class Dog
   
   attr_accessor :id, :name, :breed 
   
-  @@all = [] 
-  
   def initialize (id: nil, name:, breed:)
     @id = id 
     @name = name 
     @breed = breed 
-    @all << self 
   end
 end
